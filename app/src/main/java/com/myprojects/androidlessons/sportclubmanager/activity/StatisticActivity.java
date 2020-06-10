fed45b19f3fbca92a2b412deb0d63c04816a7f26
semyonmoroshek <s.moroseks@gmail.com>
2020-06-10 11:32:20 +0300

    38 - add active member count statistic

--- app/src/main/java/com/myprojects/androidlessons/sportclubmanager/activity/StatisticActivity.java
+++ app/src/main/java/com/myprojects/androidlessons/sportclubmanager/activity/StatisticActivity.java
@@ -1,35 +1,28 @@
 package com.myprojects.androidlessons.sportclubmanager.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.myprojects.androidlessons.sportclubmanager.R;
 import com.myprojects.androidlessons.sportclubmanager.adapter.CustomAdapter;
 import com.myprojects.androidlessons.sportclubmanager.model.Member;
-import com.myprojects.androidlessons.sportclubmanager.repository.AppDatabase;
 
 import org.parceler.Parcels;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
-
-import static com.myprojects.androidlessons.sportclubmanager.repository.AppDatabase.getInstance;
 
 public class StatisticActivity extends AppCompatActivity {
 
     public static final String EXTRA_MEMBER_LIST = "EXTRA_EMPLOYEE";
 
-    @BindView (R.id.txt_count) TextView txtMemberCount;
+    @BindView (R.id.txt_count_all_members) TextView txtMemberCount;
+    @BindView (R.id.txt_active_count) TextView txtActiveMemberCount;
 
     CustomAdapter mAdapter;
     List <Member> mMemberList = new ArrayList<>();
@@ -47,8 +40,24 @@ public class StatisticActivity extends AppCompatActivity {
     }
 
     private void setFields() {
+        setAllMemberCountField();
+        setActiveMemberCountField();
+    }
+
+    private void setActiveMemberCountField() {
+        int activeMemberCount = 0;
+
+        for(int i = 0; i < mMemberList.size(); i++ ){
+            if(!mMemberList.get(i).getMemberPaymentDate().equals("No payments")){
+                activeMemberCount++;
+            }
+        }
+        String activeMemberCountString = String.valueOf(activeMemberCount);
+        txtActiveMemberCount.setText(activeMemberCountString);
+    }
+
+    private void setAllMemberCountField() {
         String memberCount = String.valueOf(mMemberList.size());
         txtMemberCount.setText(memberCount);
-
     }
 }
